2fe0dc2abe37647bca8e38b6123767513afd255a
Chris Reeve <chris.reeve4@gmail.com>
2023-11-16 17:08:34 -0700

    Gmmk2 96 - Work change to KC_VOLD/U

--- keyboards/gmmk/gmmk2/p96/ansi/keymaps/work/keymap.c
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/work/keymap.c
@@ -35,26 +35,9 @@ void keyboard_post_init_user(void) {
 
 // Custom Keycodes
 enum keycodes {
-	VOLDN = SAFE_RANGE,
-	VOLUP,
+	PASS = SAFE_RANGE,
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-    case VOLDN:
-        if (record->event.pressed) {
-            tap_code(KC_F17);			// Volume Down
-        }
-        break;
-
-    case VOLUP:
-        if (record->event.pressed) {
-            tap_code(KC_F18);			// Volume Up
-        }
-        break;
-	}
-    return true;
-};
 
 
 
@@ -130,7 +113,7 @@ tap_dance_action_t tap_dance_actions[] = {
 
 
 // Combos
-const uint16_t PROGMEM volume[] = {VOLDN, VOLUP, COMBO_END};
+const uint16_t PROGMEM volume[] = {KC_VOLD, KC_VOLU, COMBO_END};
 
 combo_t key_combos[] = {
 	COMBO(volume, KC_F13),
@@ -143,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: Base Layer (Default Layer)
    */
 [_BL] = LAYOUT(
-  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  TD(MLT),  TD(FN),   VOLDN,    VOLUP,
+  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  TD(MLT),  TD(FN),   KC_VOLD,  KC_VOLU,
   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,   KC_PSLS,  KC_PAST,  KC_PMNS,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
   KC_F17,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   		  KC_P4,    KC_P5,    KC_P6,
